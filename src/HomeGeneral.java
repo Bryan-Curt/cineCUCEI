7c3f6b137ed8dcecc0344a84f56f5d028c2c7902
Bryan Curt <bryan.curt@univ-jfc.fr>
2019-11-20 23:09:32 -0600

    uploading home choix siege

--- src/HomeGeneral.java
+++ src/HomeGeneral.java
@@ -107,7 +107,7 @@ public class HomeGeneral extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new Jclient(); //lance l'interface client
+        new NewClass1().setVisible(true); //lance l'interface client
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
